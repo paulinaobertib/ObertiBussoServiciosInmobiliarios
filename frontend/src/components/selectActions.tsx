8d86d871dacf3205187a28b0febe1346718fe99e
Vicky Vaccarini <vicky_vaccarini@hotmail.com>
2025-04-25 20:38:50 -0300

    feat(frontend): add navbar and floating action button for property management
    This commit introduces the main navigation bar and a floating action button for CRUD operations on properties.
    - Implemented navbar
    - Added floating action button with options for property management
    
    Refs: Initial UI components for property CRUD

---
+++ frontend/src/components/selectActions.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+type SpeedDialTooltipOpenProps = {
+  onAction: (action: string) => void;
+};
+
+const actions = [
+  { icon: <AddCircleOutlineIcon />, name: 'Agregar Propiedad', action: 'create' },
+  { icon: <EditIcon />, name: 'Editar Propiedad', action: 'edit'  },
+  { icon: <DeleteIcon />, name: 'Eliminar Propiedad', action: 'delete'  },
+];
+
+export default function SpeedDialTooltipOpen({ onAction }: SpeedDialTooltipOpenProps) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Box sx={{ position: 'fixed', bottom: 24, right: 24 }}>
+
+      <SpeedDial
+        ariaLabel="SpeedDial tooltip"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SettingsIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            // tooltipOpen
+            onClick={() => {
+              handleClose();
+              onAction(action.action);
+            }}
+            FabProps={{
+              sx: {
+                bgcolor: 'primary.main',
+                color: 'white', 
+                '&:hover': {
+                  bgcolor: 'primary.dark',
+                },
+              },
+            }}
+          />
+        ))}
+      </SpeedDial>
+    </Box>
+  );
+}
