12f7afc271622a3ad59f8718f10c2a263988a5df
Vicky Vaccarini <vicky_vaccarini@hotmail.com>
2025-05-14 13:18:49 -0300

    feat(frontend): fix neighborhood issue and form details
    This commit resolves the issue related to the neighborhood selection and improves the property form details.

--- frontend/src/app/property/types/property.ts
+++ frontend/src/app/property/types/property.ts
@@ -1,6 +1,10 @@
+import { Amenity } from "./amenity";
+import { Neighborhood } from "./neighborhood";
+import { Owner } from "./owner";
+import { Type } from "./type";
+
 export interface Property {
   id: number;
-
   title: string;
   street: string;
   number: string;
@@ -8,23 +12,85 @@ export interface Property {
   status: string;
   operation: string;
   currency: string;
-
   rooms: number;
   bathrooms: number;
   bedrooms: number;
   area: number;
   price: number;
+  owner: Owner;
+  neighborhood: Neighborhood;
+  type: Type;
+  amenities: Amenity[];
+  mainImage: File | string;
+  images: File[];
+}
 
+export interface PropertyCreate {
+  title: string;
+  street: string;
+  number: string;
+  description: string;
+  status: string;
+  operation: string;
+  currency: string;
+  rooms: number;
+  bathrooms: number;
+  bedrooms: number;
+  area: number;
+  price: number;
   ownerId: number;
   neighborhoodId: number;
   typeId: number;
   amenitiesIds: number[];
-
-  mainImage: File | string; // obligatoria
-  images: File[]; // opcional
+  mainImage: File | string;
+  images: File[];
 }
 
-export type PropertyCreate = Omit<Property, "id">;
+export interface PropertyUpdate {
+  id: number;
+  title: string;
+  street: string;
+  number: string;
+  description: string;
+  status: string;
+  operation: string;
+  currency: string;
+  rooms: number;
+  bathrooms: number;
+  bedrooms: number;
+  area: number;
+  price: number;
+  ownerId: number;
+  neighborhoodId: number;
+  typeId: number;
+  amenitiesIds: number[];
+  mainImage: File | string;
+}
 
-export type PropertyUpdate = Omit<Property, "images">;
-
+export const emptyProperty: Property = {
+  id: 0,
+  title: "",
+  street: "",
+  number: "",
+  description: "",
+  status: "",
+  operation: "",
+  currency: "",
+  rooms: 0,
+  bathrooms: 0,
+  bedrooms: 0,
+  area: 0,
+  price: 0,
+  owner: { firstName: "", lastName: "", mail: "", phone: "" } as Owner,
+  neighborhood: { id: 0, name: "", city: "", type: "" } as Neighborhood,
+  type: {
+    id: 0,
+    name: "",
+    hasRooms: false,
+    hasBathrooms: false,
+    hasBedrooms: false,
+  } as Type,
+  amenities: [],
+  mainImage: "",
+  images: [],
+};
