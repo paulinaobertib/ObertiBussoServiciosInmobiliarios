d1dfa0eb98c85d28b42b1f4fda1eda4be5d7a497
Pau Oberti <paulinaobertibusso@gmail.com>
2025-05-27 11:15:35 -0300

    feat(notice): implement notice creation, update, deletion, retrieval and search
    - Created INoticeService with methods to handle creation, update, and deletion of notices.
    - Implemented retrieval of all notices and single notice by ID.
    - Added search functionality to filter notices based on text input.
    - Ensured admin-only access to create, update, and delete operations.
    
    Refs: #38, #39, #40

---
+++ backend/ms-users/src/main/java/pi/ms_users/controller/NoticeController.java
@@ -0,0 +1,51 @@
+package pi.ms_users.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import pi.ms_users.domain.Notice;
+import pi.ms_users.service.impl.NoticeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("notices")
+@RequiredArgsConstructor
+public class NoticeController {
+
+    private final NoticeService noticeService;
+
+    @PreAuthorize("hasRole('admin')")
+    @PostMapping("/create")
+    public ResponseEntity<String> create(@RequestBody Notice notice) {
+        return noticeService.create(notice);
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @PutMapping("/update")
+    public ResponseEntity<String> update(@RequestBody Notice notice) {
+        return noticeService.update(notice);
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return noticeService.delete(id);
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Notice> getById(@PathVariable Long id) {
+        return noticeService.getById(id);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Notice>> getAll() {
+        return noticeService.getAll();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Notice>> search(@RequestParam String text) {
+        return noticeService.search(text);
+    }
+}
---
+++ backend/ms-users/src/main/java/pi/ms_users/domain/Notice.java
@@ -0,0 +1,31 @@
+package pi.ms_users.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Notice")
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+}
