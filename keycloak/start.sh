8b6fa11b40da40d275e05f2a7c4ace3d742a40ae
Tomi <2102092@ucc.edu.ar>
2025-05-28 00:54:57 -0300

    feat(pipeline): WIP pipeline adjustments
    Work In Progress adjusting and experimenting with pipeline.

--- keycloak/start.sh
+++
@@ -1,8 +0,0 @@
-# 1) Asegura la carpeta H2 (montaje de tu File Share)
-mkdir -p /opt/keycloak/data/h2
-
-# 2) Elimina cualquier archivo de lock viejo
-rm -f /opt/keycloak/data/h2/*.lock.db /opt/keycloak/data/h2/DATABASECHANGELOGLOCK*
-
-# 3) Arranca Keycloak en modo dev-file
-exec /opt/keycloak/bin/kc.sh start-dev --import-realm --http-port=8080
