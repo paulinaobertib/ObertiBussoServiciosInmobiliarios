38febb7189766ba642d3b336b092601ed094f561
Tomi <2102092@ucc.edu.ar>
2025-06-10 12:27:50 -0300

    feat(pipeline): WIP pipeline adjustments
    Work In Progress adjusting and experimenting with pipeline stages.

--- backend/ms-properties/src/main/java/pi/ms_properties/configuration/azure/AzureBlobStorageConfiguration.java
+++ backend/ms-properties/src/main/java/pi/ms_properties/configuration/azure/AzureBlobStorageConfiguration.java
@@ -6,7 +6,9 @@ import com.azure.storage.blob.BlobServiceClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @Configuration
 public class AzureBlobStorageConfiguration {
 
