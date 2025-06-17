8b87776b932303baa8725542ad58668fd2b74b78
Pau Oberti <paulinaobertibusso@gmail.com>
2025-06-17 11:38:17 -0300

    feat(recommendation): start implementing hybrid recommendation logic for user notifications
    - Began development of automatic property notification system based on user preferences.
    - Designed initial structure for hybrid recommendation engine (content-based + collaborative).
    - Linked preference data to trigger personalized notifications.
    
    Refs: #34

---
+++ backend/ms-properties/src/main/java/pi/ms_properties/domain/UserView.java
@@ -0,0 +1,28 @@
+package pi.ms_properties.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "UserView")
+@Setter
+public class UserView {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id", nullable = false)
+    @JsonIgnoreProperties({"owner", "neighborhood", "type", "amenities", "images", "inquiries"})
+    private Property property;
+}
---
+++ backend/ms-properties/src/main/java/pi/ms_properties/recommendation/python/MLClient.java
@@ -0,0 +1,4 @@
+package pi.ms_properties.recommendation.python;
+
+public class MLClient {
+}
