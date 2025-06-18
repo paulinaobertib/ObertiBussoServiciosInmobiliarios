e4509a41a0d3b6e1bec3b41e3979d1edbe641b33
Pau Oberti <paulinaobertibusso@gmail.com>
2025-06-18 15:32:18 -0300

    test(ms-users): completed and validated tests
    Refs: #4

--- backend/ms-users/src/test/java/pi/ms_users/controllerTest/FavoriteControllerTest.java
+++ backend/ms-users/src/test/java/pi/ms_users/controllerTest/FavoriteControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import pi.ms_users.controller.FavoriteController;
 import pi.ms_users.domain.Favorite;
@@ -108,6 +109,20 @@ public class FavoriteControllerTest {
                 .andExpect(jsonPath("$[0].id").value(1));
     }
 
+    @Test
+    void getAllUsers_withAdminRole_shouldReturnListOfUsers() throws Exception {
+        List<String> users = List.of("user1", "user2");
+
+        when(favoriteService.findAllUsers()).thenReturn(users);
+
+        mockMvc.perform(get("/favorites/internal/allUser")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_admin"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$[0]").value("user1"))
+                .andExpect(jsonPath("$[1]").value("user2"));
+    }
+
     // casos de error
 
     @Test
@@ -180,5 +195,12 @@ public class FavoriteControllerTest {
         mockMvc.perform(get("/favorites/property/999")
                         .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_admin"))))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getAllUsers_withoutAdminRole_shouldReturnForbidden() throws Exception {
+        mockMvc.perform(get("/favorites/internal/allUser")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_user"))))
+                .andExpect(status().isForbidden());
     }
 }
