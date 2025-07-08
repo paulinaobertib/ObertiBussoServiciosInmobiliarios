58f5d2c868075bbf3fe88e9b0161b37744937369
Pau Oberti <paulinaobertibusso@gmail.com>
2025-07-07 23:49:20 -0300

    feat(ms-properties): add PropertyComparer component and integrate into ComparePage
    - Developed PropertyComparer component to display property comparisons.
    - Added the component to ComparePage for seamless user access.
    - Improved UI flow for property comparison feature.
    
    Refs: #69

--- frontend/src/pages/ComparePage.tsx
+++ frontend/src/pages/ComparePage.tsx
@@ -7,6 +7,8 @@ import { Modal } from '../app/shared/components/Modal';
 import { InquiriesPanel } from '../app/property/components/inquiries/InquiriesPanel';
 import { useState } from 'react';
 import { ROUTES } from '../lib';
+import { PropertyDTOAI } from '../app/property/types/property';
+import { Comparer } from '../app/property/components/comparer/Comparer';
 
 const Compare = () => {
   const theme = useTheme();
@@ -26,6 +28,21 @@ const Compare = () => {
 
   const { selectedPropertyIds } = usePropertyCrud();
 
+  const comparisonDataAI: PropertyDTOAI[] = comparisonItems.map((property) => ({
+    address: `${property.street} ${property.number}, ${property.neighborhood.name}, ${property.neighborhood.city}, Argentina`,
+    latitude: 0,
+    longitude: 0,
+    rooms: property.rooms,
+    bathrooms: property.bathrooms,
+    bedrooms: property.bedrooms,
+    area: property.area,
+    coveredArea: property.coveredArea,
+    price: property.price,
+    operation: property.operation,
+    type: property.type.name,
+    amenities: new Set(property.amenities.map((a) => a.name)),
+  }));
+
   return (
     <BasePage maxWidth={false}>
       <Box sx={{ display: 'flex', justifyContent: 'flex-start', mt: 2, mb: -4 }}>
@@ -36,6 +53,8 @@ const Compare = () => {
 
       <>
         <PropertyDetailsCompare comparisonItems={comparisonItems} />
+
+        <Comparer data={comparisonDataAI} />
 
         <Box sx={{ display: 'flex', justifyContent: 'center', pb: 8 }}>
           <Button
