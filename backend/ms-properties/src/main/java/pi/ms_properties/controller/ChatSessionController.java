d41748e5486f404eef601038b4c881839606661a
Pau Oberti <paulinaobertibusso@gmail.com>
2025-08-01 18:19:42 -0300

    feat: some backend and frontend little changes

--- backend/ms-properties/src/main/java/pi/ms_properties/controller/ChatSessionController.java
+++ backend/ms-properties/src/main/java/pi/ms_properties/controller/ChatSessionController.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import pi.ms_properties.domain.ChatSession;
 import pi.ms_properties.dto.ChatSessionDTO;
+import pi.ms_properties.dto.ChatSessionGetDTO;
 import pi.ms_properties.service.interf.IChatSessionService;
 
 import java.util.List;
@@ -30,13 +30,13 @@ public class ChatSessionController {
 
     @PreAuthorize("hasRole('admin')")
     @GetMapping("/getById/{id}")
-    public ResponseEntity<ChatSession> getById(@PathVariable Long id) {
+    public ResponseEntity<ChatSessionGetDTO> getById(@PathVariable Long id) {
         return ResponseEntity.ok(chatSessionService.getById(id));
     }
 
     @PreAuthorize("hasRole('admin')")
     @GetMapping("/getAll")
-    public ResponseEntity<List<ChatSession>> getAll() {
+    public ResponseEntity<List<ChatSessionGetDTO >> getAll() {
         return ResponseEntity.ok(chatSessionService.getAll());
     }
 }
