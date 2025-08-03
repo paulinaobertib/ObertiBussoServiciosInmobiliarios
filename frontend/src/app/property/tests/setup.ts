26517e397c88455bd0bee61152fb8f7f2f4c3c90
Pau Oberti <paulinaobertibusso@gmail.com>
2025-08-03 16:20:20 -0300

    feat: thesis submission - August 4th delivery

--- frontend/src/app/property/tests/setup.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-
-import { afterEach } from "vitest";
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-afterEach(() => {
-    cleanup();
-});
--- frontend/src/app/property/types/image.ts
+++
@@ -1,2 +0,0 @@
-export type Image = File | string;
-
