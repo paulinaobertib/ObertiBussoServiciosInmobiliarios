2f42e01e622608c75adff414a40f3e6e77149fd4
Tomi Cassanelli <tomascassanelli02@gmail.com>
2025-09-30 20:42:33 -0300

    feat(pipeline): WIP pipeline adjustments

--- backend/python/train_utils.py
+++ backend/python/train_utils.py
@@ -3,9 +3,21 @@ from sqlalchemy import create_engine
 from surprise import Dataset, Reader, SVD, accuracy
 import joblib
 import os
+from urllib.parse import quote_plus
+
+def get_db_url():
+    user = os.getenv("DB_SQL_USERNAME", "root")
+    password = quote_plus(os.getenv("DB_SQL_PASSWORD", "")) 
+    host = os.getenv("DB_SQL_HOST", "localhost")
+    port = os.getenv("DB_SQL_PORT", "3306")
+    name = os.getenv("DB_SQL_NAME_PROD", "obertibussoserviciosinmobiliarios")
+
+    suffix = f"?sslMode=REQUIRED&serverTimezone=UTC"
+
+    return f"mysql+pymysql://{user}:{password}@{host}:{port}/{name}{suffix}"
 
 def entrenar_modelo():
-    engine = create_engine('mysql+pymysql://root:lmcpauli1@localhost:3306/obertibussoserviciosinmobiliarios')
+    engine = create_engine(get_db_url())
 
     query = """
     SELECT user_id, property_id, 1 AS rating
